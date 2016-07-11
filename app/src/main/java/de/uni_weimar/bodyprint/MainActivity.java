b785e39daee1d0d9f478e8e279d10cb3befe764c
sw_project <sw_project@example.com>
2016-07-11 13:36:52 +0200

    printing bodypart data

--- app/src/main/java/de/uni_weimar/bodyprint/MainActivity.java
+++ app/src/main/java/de/uni_weimar/bodyprint/MainActivity.java
@@ -1,46 +1,123 @@
 package de.uni_weimar.bodyprint;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfDMatch;
-import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.Scalar;
-import org.opencv.features2d.DescriptorExtractor;
-import org.opencv.features2d.DescriptorMatcher;
-import org.opencv.features2d.Features2d;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.features2d.FeatureDetector;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import de.uni_weimar.bodyprint.BusinessLogic.Controller;
-import de.uni_weimar.bodyprint.Model.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "Main";
 
     static{
         System.loadLibrary("opencv_java3");
     }
+
+    TextView outText;
+    Button printPositionButton;
+    Button printTestButton;
+
     Controller controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        controller = new Controller(this);
-        Bitmap query = BitmapFactory.decodeResource(getResources(), R.drawable.query3);
-        User user = controller.scan(query);
-        Toast.makeText(this, (String)user.getName(),Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+//        controller = new Controller(this);
+//        Bitmap query = BitmapFactory.decodeResource(getResources(), R.drawable.query3);
+//        User user = controller.scan(query);
+//        Toast.makeText(this, (String) user.getName(), Toast.LENGTH_LONG).show();
+
+        outText = (TextView) findViewById(R.id.text_out);
+
+        printPositionButton = (Button) findViewById(R.id.button_position);
+        printPositionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                outText.setText(getProcData("absolute_position_reporting"));
+            }
+        });
+
+        printTestButton = (Button) findViewById(R.id.button_test);
+        printTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                outText.setText(getProcData("test_reporting"));
+            }
+        });
+    }
+
+    String getProcData(String fileName){
+
+        Runtime runtime = Runtime.getRuntime();
+        Process proc = null;
+        OutputStreamWriter osw = null;
+
+        String command="/system/bin/cat /proc/"+fileName;
+
+        try { // Run Script
+            proc = runtime.exec("su");
+            osw = new OutputStreamWriter(proc.getOutputStream());
+            osw.write(command);
+            osw.flush();
+            osw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (osw != null) {
+                try {
+                    osw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            if (proc != null) {
+                proc.waitFor();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return getStringFromInputStream(proc.getInputStream());
+    }
+
+    private static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+
     }
 
 
--- openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/befi8957/StudioProjects/BodyPrint/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/projectsw/eardentification/BodyPart/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
